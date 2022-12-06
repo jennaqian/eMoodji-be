8b07894be140a245883153ba0f01e489d5bb0684
Jenna Qian <jiayin1996@gmail.com>
2022-12-05 19:37:24 -0500

    Added port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require("./app.js");
 
 require("dotenv").config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => {
   console.log(`🎧 Listening on port ${PORT} 🎧 `);
